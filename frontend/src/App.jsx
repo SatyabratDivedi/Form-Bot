68914cf05c267a81bd0f6175592fe4f8adf0594b
Satyabrat Divedi <raghavstar1991@gmail.com>
2024-07-21 17:30:11 +0530

    Created frontend and backend folders

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MainDashboard from "./components/mainDashboard/MainDashboard.jsx";
+
+const App = () => {
+  return (
+    <div>
+      <MainDashboard />
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/components/mainDashboard/MainDashboard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import style from "./mainDashboard.module.css";
+
+const MainDashboard = () => {
+  return (
+    <>
+      <div>
+        <header className={style.header}>
+          <img src='' alt="" />
+        </header>
+      </div>
+    </>
+  );
+};
+
+export default MainDashboard;
